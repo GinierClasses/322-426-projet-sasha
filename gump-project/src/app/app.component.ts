5410f0c8e7d7a915581f228dc45d67887fb91f5b
Bastien Jaccard info2e <118716108+Bdeeux@users.noreply.github.com>
2023-05-08 12:10:24 +0200

    share feature branch

--- gump-project/src/app/app.component.ts
+++ gump-project/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import {Component} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {ShareComponent} from "./share/share.component";
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,33 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'gump-project';
+  svgIcons = ['success', 'error', 'warning', 'info', 'instagram', 'linkedin', 'facebook'];
+  constructor(
+    public dialog: MatDialog,
+ //   private alertService: AlertService,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    for (const icon of this.svgIcons) {
+      this.matIconRegistry.addSvgIcon(icon,
+        this.domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/" + icon + ".svg")
+      );
+    }
+  }
+
+ // test() {
+ //   this.alertService.alert('test', 'test', 'info');
+ // }
+
+  openShareWindow(): void {
+    const dialogRef = this.dialog.open(ShareComponent, {
+      width: '100%',
+      height: '95%',
+      maxHeight: '100%'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed : ' + result);
+    });
+  }
 }
--- gump-project/src/app/app.module.ts
+++ gump-project/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ShareComponent } from './share/share.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import {FormsModule} from "@angular/forms";
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ShareComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
