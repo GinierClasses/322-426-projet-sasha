79886e19ddb9f89f84b71bd01f56bf2aa6f87a52
t0smo <118718098+t0smo@users.noreply.github.com>
2023-05-08 13:01:12 +0200

    Home page avec un début du contenu.

--- gump-project/src/app/app.module.ts
+++ gump-project/src/app/app.module.ts
@@ -9,12 +9,15 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatIconModule} from "@angular/material/icon";
 import {HttpClientModule} from "@angular/common/http";
-import { HomeComponent } from './home/home.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatGridListModule} from '@angular/material/grid-list';
-import { MatSidenavModule} from '@angular/material/sidenav';
-import { MatFormFieldModule} from '@angular/material/form-field';
-import { MatSelectModule} from "@angular/material/select";
+import {HomeComponent } from './home/home.component';
+import {MatToolbarModule } from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from "@angular/material/select";
+import {MatCardModule} from "@angular/material/card";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
@@ -35,6 +38,9 @@ import { MatSelectModule} from "@angular/material/select";
     MatSidenavModule,
     MatFormFieldModule,
     MatSelectModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
 
   ],
   providers: [],
